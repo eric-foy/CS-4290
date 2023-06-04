9d76940a14e6dbbf2e87b5da1b78ff2c08eee7d6
Eric Foy <foy.8@wright.edu>
2023-06-04 16:21:07 -0400

    printout # of elements in zn

--- all_inverts_in_zn.py
+++ all_inverts_in_zn.py
@@ -2,9 +2,12 @@ import sys
 import gcd
 
 def check(n):
+    c = 0
     for i in range(1, n):
         if (gcd.gcd(i, n) == 1):
+            c += 1
             print(i)
+    print("Number in Zn:", c)
 
 if __name__ == "__main__":
     check(int(sys.argv[1]))

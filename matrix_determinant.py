0ecbfe18748330dfc712e9ce249d8585827b2a7c
Eric Foy <foy.8@wright.edu>
2023-06-04 23:18:10 -0400

    matrix determinant now for modular numbers

--- matrix_determinant.py
+++ matrix_determinant.py
@@ -1,7 +1,7 @@
 import matrix_minors as mm
 # negate if m[i][j] is even?
 
-def determinant(A):
+def determinant(A, Zn):
     # should be square
     n = len(A)
     for i in A:
@@ -14,19 +14,17 @@ def determinant(A):
 
     x = 0
     for i in range(0, n):
-        #print(A[0][i])
-        #print((-1)**i)
-        #print(determinant(mm.minors(A, 1, i+1)))
-        x += A[0][i] * (-1)**i * determinant(mm.minors(A, 1, i+1))
+        x += A[0][i] * (-1)**i * determinant(mm.minors(A, 1, i+1), Zn)
+        x %= Zn
     return x
 
 if __name__ == "__main__":
-    # 2 by 2 should be -2
+    # 2 by 2 should be 10-2
     A = [[1, 2], [3, 4]]
-    print(determinant(A))
-    # 3 by 3 should be -306
+    print(determinant(A, 10))
+    # 3 by 3 should be 1000-306
     A = [[6, 1, 1], [4, -2, 5], [2, 8, 7]]
-    print(determinant(A))
-    # 4 by 4 should be -270
+    print(determinant(A, 1000))
+    # 4 by 4 should be 1000-270
     A = [[6, 1, 1, 5], [4, -2, 5, 1], [2, 8, 7, 7], [3, 4, 1, 6]]
-    print(determinant(A))
+    print(determinant(A, 1000))

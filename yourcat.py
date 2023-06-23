7386809a908ce46cefdc8f5b296031138ebb78d1
Eric Foy <foy.8@wright.edu>
2023-06-23 13:33:50 -0400

    yourcat now encrypt

--- yourcat.py
+++ yourcat.py
@@ -1,17 +1,29 @@
 import sys
 
-def deexamcipher(msg):
+def encrypt(p):
+    y1 = ""
+    for i in range(0, len(p), 2):
+        y1 += p[i]
+    y2 = ""
+    for i in range(1, len(p), 2):
+        y2 += p[i]
+    y = y1 + y2
+    return y
+
+def decrypt(msg):
     n = int(len(msg)/2)
-    a = msg[:n+1]
-    b = msg[n+1:]
+    if (len(msg) % 2 == 0):
+        a = msg[:n]
+        b = msg[n:]
+    else:
+        a = msg[:n+1]
+        b = msg[n+1:]
     print(a)
     print(b)
     o = ""
     for i in range(0, n):
         o += a[i]
         o += b[i]
-    o += a[-1]
-    print(o.lower())
-
-if __name__ == "__main__":
-    deexamcipher(sys.argv[1])
+    if (len(msg) % 2 == 1):
+        o += a[-1]
+    return o.lower()

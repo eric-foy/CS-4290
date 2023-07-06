11e30696e3cc89dc719b3b9dec36b49df8042f2b
Eric Foy <foy.8@wright.edu>
2023-07-05 20:25:17 -0400

    is x square in Zn

--- jocobi.py
+++ jocobi.py
@@ -15,12 +15,48 @@ def jocobi(a, b):
     else:
         return int((-1)**(((a-1)/2)*((b-1)/2))) * jocobi(b, a)
 
+def legendre(a, p):
+    e = (p - 1)/2
+    o = a**e
+    o %= p
+    return o
+
+def isSquare(a, Zn):
+    l = legendre(a, Zn)
+    if (l != 1):
+        print("legendre not equal to 1, maybe ", Zn, " not odd prime")
+    return l
+
+def squares(Zn, check=True):
+    o = set()
+    for i in range(1, Zn):
+        x = i**2
+        x %= Zn
+        o.add(x)
+    if check:
+        for i in o:
+            isSquare(i, Zn)
+    return o
+
+def roots(a, Zn):
+    o = []
+    for i in range(1, Zn):
+        x = i**2
+        x %= Zn
+        if x == a:
+            o += [i]
+
+    return o
+
 def test():
     print(jocobi(51, 133))
     print(jocobi(31, 51))
     print(jocobi(2, 75))
     print(jocobi(19, 101))
     print(jocobi(54, 101))
+
+    print(squares(7))
+    print(roots(4, 7))
 
 if __name__ == "__main__":
     print(jocobi(int(sys.argv[1]), int(sys.argv[2])))

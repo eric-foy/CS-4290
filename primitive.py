284bc8f25950ae2c086dafa00aa64b10446b8e6b
Eric Foy <foy.8@wright.edu>
2023-07-12 18:27:49 -0400

    disp steps in primitive calculations

--- primitive.py
+++ primitive.py
@@ -1,15 +1,23 @@
 from divmul import power
 
-def isPrimitive(a, Zn):
+def isPrimitive(a, Zn, disp=False):
     Z = set(range(1, Zn))
     x = set()
     y = power(a, 1, Zn)
+    if disp:
+        print(f"{a} = {y}")
     x.add(y)
     c = 2
     while y != 1:
         y = power(a, c, Zn)
+        if disp:
+            print(f"{a}^{c} = {y}")
         x.add(y)
         c += 1
+
+    if disp:
+        print(f"Z={Z}")
+        print(f"x={x}")
 
     if x == Z:
         return True
@@ -18,16 +26,16 @@ def isPrimitive(a, Zn):
 
 
 if __name__ == "__main__":
-    print(isPrimitive(1, 3))
-    print(isPrimitive(2, 3))
+    print(isPrimitive(1, 3, True))
+    print(isPrimitive(2, 3, True))
 
     print()
-    print(isPrimitive(1, 7))
-    print(isPrimitive(2, 7))
-    print(isPrimitive(3, 7))
-    print(isPrimitive(6, 7))
+    print(isPrimitive(1, 7, True))
+    print(isPrimitive(2, 7, True))
+    print(isPrimitive(3, 7, True))
+    print(isPrimitive(6, 7, True))
 
     print()
-    print(isPrimitive(2, 19))
-    print(isPrimitive(3, 19))
-    print(isPrimitive(4, 19))
+    print(isPrimitive(2, 19, True))
+    print(isPrimitive(3, 19, True))
+    print(isPrimitive(4, 19, True))
